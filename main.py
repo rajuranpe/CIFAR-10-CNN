da26c3b04b2fdf59493039a53a767098ab2fbed4
Rami Ilo <37746272+rajuranpe@users.noreply.github.com>
2019-04-08 00:04:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,20 @@ import keras
 import utils
 import createNetwork
 
-model = utils.loadModel()
-createNetwork.runNetwork(model, 64, 10)
+choice = input("Type \"n\" to create a new network or \"t\" to continue training an existing one: ")
+if choice == "n":
+    createNetwork.createNetwork()
+
+if choice == "t":
+    while True:
+        try:
+            epochs = int(input("How many epochs? Recommended 5-30 "))
+            break
+        except ValueError:
+            print("Please give a positive whole number.")
+
+    model = utils.loadModel()
+    createNetwork.runNetwork(model, 64, epochs)
+
+else:
+    "Please read the instruction message."
